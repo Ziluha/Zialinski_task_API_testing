eaf4da5aaef15e7bdbdbd30487e265a8fe81b6e7
ICX\ivan.zialinski <ivan.zialinski@gmail.com>
2018-01-03 17:04:11 +0300

    Commit with my tryings

--- Zialinski_task_API_testing/OAuth2Helper.cs
+++ Zialinski_task_API_testing/OAuth2Helper.cs
@@ -16,6 +16,11 @@ namespace Zialinski_task_API_testing
             return elem;
         }
 
+        public static string GetSpecificValueFromCookies(IRestResponse response, string specificName)
+        {
+            return response.Cookies.First(item => item.Name == specificName).Value;
+        }
+
         public static string GetToken(string url, string username, string password, string specialCookie, string specialParam)
         {
             var client = new RestClient(url);
@@ -24,7 +29,7 @@ namespace Zialinski_task_API_testing
 
             IRestResponse getResponse = client.Execute(getRequest);
 
-            string specialParamValue = getResponse.Cookies[0].Value;
+            string specialParamValue = GetSpecificValueFromCookies(getResponse, specialCookie);
 
             var postRequest = new RestRequest(Method.POST);
             postRequest.AddQueryParameter("response_type", "token");
@@ -42,5 +47,40 @@ namespace Zialinski_task_API_testing
             return token;
         }
 
+        public static string GetTokenThrowCode(string url, string tokenPath, string username, string password, string specialCookie, string specialParam)
+        {
+            var client = new RestClient(url);
+            var getRequest = new RestRequest(Method.GET);
+            getRequest.AddQueryParameter("response_type", "code");
+
+            IRestResponse getResponse = client.Execute(getRequest);
+
+            string specialParamValue = getResponse.Cookies[0].Value;
+
+            var postRequest = new RestRequest(Method.POST);
+            postRequest.AddQueryParameter("response_type", "code");
+            postRequest.AddCookie(specialCookie, specialParamValue);
+            postRequest.AddHeader("cache-control", "no-cache");
+            postRequest.AddHeader("content-type", "application/x-www-form-urlencoded");
+            postRequest.AddParameter("application/x-www-form-urlencoded",
+                $"username={username}&password={password}&{specialParam}={specialParamValue}",
+                ParameterType.RequestBody);
+
+            IRestResponse postResponse = client.Execute(postRequest);
+
+            string code = GetUriElement(postResponse.ResponseUri.Query, "code");
+
+            string tokenUri = postResponse.ResponseUri.AbsoluteUri.Replace(postResponse.ResponseUri.Query, tokenPath);
+
+            var clientForToken = new RestClient(tokenUri);
+            var postForToken = new RestRequest(Method.POST);
+            postForToken.AddQueryParameter("code", code);
+
+            IRestResponse postResponseForToken = clientForToken.Execute(postForToken);
+
+            // here request to /oauth2/token
+
+            return code;
+        }
     }
 }
--- Zialinski_task_API_testing/OAuthAPITest.cs
+++ Zialinski_task_API_testing/OAuthAPITest.cs
@@ -19,13 +19,13 @@ namespace Zialinski_task_API_testing
     class OAuthAPITest
     {
         [Test]
-        public void GetMyImages()
+        public void ImplicitFrow()
         {
-            string token = OAuth2Helper.GetToken("https://api.imgur.com/oauth2/authorize?client_id=2058b576d9029e1",
+            string token = OAuth2Helper.GetToken("https://api.imgur.com/oauth2/authorize?client_id=0f624d4e6985af4",
                 "TestTaskZel", "Test1234Test", "authorize_token", "allow");
             var client = new RestClient("https://api.imgur.com/3/account/me/images");
+            client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(token, "Bearer");
             var getRequest = new RestRequest(Method.GET);
-            getRequest.AddHeader("Authorization", "Bearer " + token);
             IRestResponse getResponse = client.Execute(getRequest);
 
             JSchema schema = JSchema.Parse(@"{
@@ -46,5 +46,68 @@ namespace Zialinski_task_API_testing
             Assert.True(valid, "Result is not valid");
         }
 
+        [Test]
+        public void ExplicitFlow()
+        {
+           /* var client2 = new RestClient("https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=ZAM34PJ47M66YCYJ6J");
+
+            var gettttt = new RestRequest(Method.GET);
+            IRestResponse getttRes = client2.Execute(gettttt);*/
+
+            var client = new RestClient("https://www.wunderlist.com/oauth/authorize?client_id=47751cb0c02675e3621a&redirect_uri=https://www.getpostman.com/oauth2/callback&state=RANDOM");
+
+            /*var postCredentials = new RestRequest("ajax/login/", Method.POST);
+            postCredentials.AddHeader("content-type", "application/x-www-form-urlencoded");
+            postCredentials.AddParameter("application/x-www-form-urlencoded",
+                "email=test.task.zel@gmail.com&password=Test1234Test&referrer=/oauth/authorize?response_type=code&client_id=ZAM34PJ47M66YCYJ6J",
+                ParameterType.RequestBody);
+
+            IRestResponse credsResponse = client.Execute(postCredentials);
+            string specValue = OAuth2Helper.GetSpecificValueFromCookies(credsResponse, "csrftoken");
+
+            var postAuth = new RestRequest("ngapi/oauth/authorizing", Method.POST);
+            postAuth.AddHeader("Cookie", credsResponse.Headers[12].Value.ToString());
+            postAuth.AddCookie("csrftoken", specValue);
+            postAuth.AddHeader("content-type", "application/x-www-form-urlencoded");
+            postAuth.AddParameter("application/x-www-form-urlencoded",
+                $"csrfmiddlewaretoken={specValue}&client_id=ZAM34PJ47M66YCYJ6J&response_type=code&access_choices=allow",
+                ParameterType.RequestBody);
+            IRestResponse authResponse = client.Execute(postAuth);
+            
+            Assert.True(true);*/
+
+            var postCredentials = new RestRequest(Method.POST);
+            postCredentials.AddHeader("content-type", "application/x-www-form-urlencoded");
+            postCredentials.AddParameter("application/x-www-form-urlencoded",
+                "email=test.task.zel@gmail.com&password=Test1234Test",
+                ParameterType.RequestBody);
+
+            IRestResponse credsResponse = client.Execute(postCredentials);
+
+
+            //string specialParamValue = getResponse.Cookies[0].Value;
+
+            /* var postRequest = new RestRequest(Method.POST);
+             postRequest.AddQueryParameter("response_type", "code");
+             postRequest.AddCookie(specialCookie, specialParamValue);
+             postRequest.AddHeader("cache-control", "no-cache");
+             postRequest.AddHeader("content-type", "application/x-www-form-urlencoded");
+             postRequest.AddParameter("application/x-www-form-urlencoded",
+                 $"username={username}&password={password}&{specialParam}={specialParamValue}",
+                 ParameterType.RequestBody);
+ 
+             IRestResponse postResponse = client.Execute(postRequest);
+ 
+             string code = GetUriElement(postResponse.ResponseUri.Query, "code");
+ 
+             string tokenUri = postResponse.ResponseUri.AbsoluteUri.Replace(postResponse.ResponseUri.Query, tokenPath);
+ 
+             var clientForToken = new RestClient(tokenUri);
+             var postForToken = new RestRequest(Method.POST);
+             postForToken.AddQueryParameter("code", code);
+ 
+             IRestResponse postResponseForToken = clientForToken.Execute(postForToken);*/
+
+        }
     }
 }
